a3456f4b20668a078bb38f8a859ee3c5926e8f08
Tim <timspitters@hotmail.nl>
2025-03-13 12:52:17 +0100

    Create Gateway project and Front-end web app

---
+++ Front-end/TSWMS.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TSWMS.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
