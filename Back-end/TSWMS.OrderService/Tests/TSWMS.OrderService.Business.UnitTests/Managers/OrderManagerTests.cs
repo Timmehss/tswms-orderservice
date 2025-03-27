8c472e41c6fd95bcb81c5503e7b6023a99cada5d
Tim <timspitters@hotmail.nl>
2025-03-27 15:45:39 +0100

    Add UnitTests to Business and Data projects

--- Back-end/TSWMS.OrderService/Tests/TSWMS.OrderService.Business.UnitTests/Managers/OrderManagerTests.cs
+++ Back-end/TSWMS.OrderService/Tests/TSWMS.OrderService.Business.UnitTests/Managers/OrderManagerTests.cs
@@ -1,13 +1,72 @@
 ﻿#region Usings
 
+using Moq;
+using TSWMS.OrderService.Business.Managers;
+using TSWMS.OrderService.Shared.Interfaces;
+using TSWMS.OrderService.Shared.Models;
+
 #endregion
 
 namespace TSWMS.OrderService.Business.UnitTests;
 
 public class OrderManagerTests
 {
+    private readonly OrderManager _orderManager;
+    private readonly Mock<IOrderRepository> _orderRepositoryMock;
+
     public OrderManagerTests()
     {
+        // Mock the IOrderRepository
+        _orderRepositoryMock = new Mock<IOrderRepository>();
 
+        // Initialize OrderManager with the mocked repository
+        _orderManager = new OrderManager(_orderRepositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task GetOrders_ShouldReturnOrdersFromRepository()
+    {
+        // Arrange
+        var expectedOrders = new List<Order>
+        {
+            new Order
+            {
+                OrderId = Guid.Parse("a3c99b75-b0a5-4a3b-9c8c-34eed285f269"),
+                UserId = Guid.Parse("52348777-7a0e-4139-9489-87dff9d47b7e"),
+                TotalAmount = 160.00m,
+                OrderDate = new DateTime(2024, 7, 15),
+                OrderItems = new List<OrderItem>
+                {
+                    new OrderItem
+                    {
+                        OrderId = Guid.Parse("a3c99b75-b0a5-4a3b-9c8c-34eed285f269"),
+                        ProductId = Guid.Parse("de3c9457-f6a8-4b4b-a4c1-f9d3db6f1e1d"),
+                        Quantity = 1,
+                        Price = 30.00m
+                    }
+                }
+            }
+        };
+
+        _orderRepositoryMock.Setup(repo => repo.GetOrders()).ReturnsAsync(expectedOrders);
+
+        // Act
+        var orders = await _orderManager.GetOrders();
+
+        // Assert
+        Assert.NotNull(orders);
+        Assert.Equal(expectedOrders.Count, orders.Count());
+        var firstOrder = orders.First();
+        Assert.NotNull(firstOrder);
+        Assert.Equal(expectedOrders.First().OrderId, firstOrder.OrderId);
+        Assert.Single(firstOrder.OrderItems);
+
+        var firstOrderItem = firstOrder.OrderItems.First();
+        Assert.Equal(expectedOrders.First().OrderItems.First().ProductId, firstOrderItem.ProductId);
+        Assert.Equal(expectedOrders.First().OrderItems.First().Quantity, firstOrderItem.Quantity);
+        Assert.Equal(expectedOrders.First().OrderItems.First().Price, firstOrderItem.Price);
+
+        // Verify that the GetOrders method on the repository was called exactly once
+        _orderRepositoryMock.Verify(repo => repo.GetOrders(), Times.Once);
     }
 }
