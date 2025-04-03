40e4f329442106a11b9ac00b369f385f8c568a67
Tim <timspitters@hotmail.nl>
2025-04-03 10:09:46 +0200

    Split services to new repos

--- Back-end/TSWMS.ProductService/TSWMS.ProductService.Shared/Models/Product.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TSWMS.ProductService.Shared.Models;
-
-public class Product
-{
-    public Guid ProductId { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public int AvailableStock { get; set; }
-}
--- Back-end/TSWMS.UserService/TSWMS.UserService.Shared/Models/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TSWMS.UserService.Shared.Models;
-
-public class User
-{
-    public Guid UserId { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-}
